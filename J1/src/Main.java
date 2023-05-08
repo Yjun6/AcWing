34c2fe8b665eab5d84168849e9e1abcef77d8c3b
yjun6 <Su_Xi_03@outlook.com>
2023-05-08 19:11:54 +0800

    AcWing_J1

---
+++ J1/src/Main.java
@@ -0,0 +1,17 @@
+/**
+ * Created with IntelliJ IDEA.
+ * Description:
+ * User:${USER}
+ * Date:${YEAR}-${MONTH}-${DAY}
+ * Time:${TIME}
+ */
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        System.out.print(a+b);
+    }
+}
